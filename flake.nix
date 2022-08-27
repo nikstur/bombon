300bf620a3eaa3857237a3c91da782409dd32854
nikstur <nikstur@outlook.com>
2022-08-27 17:11:39 +0200

    pre-commit: init

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, pre-commit-hooks }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -21,8 +22,21 @@
           default = transformer;
         };
 
+        checks = {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
+              # Rustfmt and clippy are not included here because these hooks
+              # don't work when the rust project is in a subdirectory
+            };
+          };
+        };
+
         devShells.default = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt rustPackages.clippy ];
+          inherit (self.checks.${system}.pre-commit) shellHook;
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
