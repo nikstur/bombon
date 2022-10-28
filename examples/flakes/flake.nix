e6de64a01e29d4cfb96ae8dbc55242efeaf044b2
nikstur <nikstur@outlook.com>
2022-10-28 23:27:53 +0200

    examples.flakes: remove eachDefaultSystem

--- examples/flakes/flake.nix
+++ examples/flakes/flake.nix
@@ -1,16 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
     bombon.url = "github:nikstur/bombon";
+    bombon.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, bombon }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = bombon.lib.${system}.buildBom pkgs.hello;
-      });
+  outputs = { self, nixpkgs, bombon }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = bombon.lib.${system}.buildBom pkgs.hello;
+    };
 }
