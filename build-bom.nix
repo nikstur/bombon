27e6b322f5f084bb490cf54057d8b7cac5005a83
nikstur <nikstur@outlook.com>
2022-12-19 00:27:33 +0100

    buildBom: add includeBuildtimDependencies option

--- build-bom.nix
+++ build-bom.nix
@@ -1,10 +1,20 @@
-{ runCommand
+{ lib
+, runCommand
 , transformer
 , buildtimeDependencies
 , runtimeDependencies
 }:
 
-drv:
+drv: { includeBuildtimeDependencies ? false }:
+let
+  flags = [ ] ++ lib.optionals includeBuildtimeDependencies [
+    "--include-buildtime-dependencies"
+  ];
+in
 runCommand "${drv.name}.cdx.json" { buildInputs = [ transformer ]; } ''
-  bombon-transformer ${drv} ${buildtimeDependencies drv} ${runtimeDependencies drv} > $out
+  bombon-transformer ${drv} \
+    ${toString flags} \
+    ${buildtimeDependencies drv} \
+    ${runtimeDependencies drv} > $out
 ''
+
