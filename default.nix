1db53aba8faf381a83eb06ab71df5abcc6fefc46
Uluc Sengil <uluc.sengil@helsing.ai>
2026-07-08 13:37:28 +0200

    Use a scope for overridability

--- default.nix
+++ default.nix
@@ -1,25 +1,20 @@
 { pkgs }:
-
-let
+pkgs.lib.makeScope pkgs.newScope (self: {
   passthruVendoredSbom = import ./nix/passthru-vendored.nix;
-  transformerWithoutSbom = pkgs.callPackage ./nix/packages/transformer.nix { };
-in
-rec {
 
   # It's useful to have these exposed for debugging. However, they are not a
   # public interface.
   __internal = {
-    buildtimeDependencies = pkgs.callPackage ./nix/buildtime-dependencies.nix { };
-    runtimeDependencies = pkgs.callPackage ./nix/runtime-dependencies.nix { };
+    buildtimeDependencies = self.callPackage ./nix/buildtime-dependencies.nix { };
+    runtimeDependencies = self.callPackage ./nix/runtime-dependencies.nix { };
+    transformerWithoutSbom = self.callPackage ./nix/packages/transformer.nix { };
   };
 
-  transformer = passthruVendoredSbom.rust transformerWithoutSbom { inherit pkgs; };
-
-  buildBom = pkgs.callPackage ./nix/build-bom.nix {
-    inherit transformer;
-    inherit (__internal) buildtimeDependencies runtimeDependencies;
+  transformer = self.passthruVendoredSbom.rust self.__internal.transformerWithoutSbom {
+    inherit pkgs;
   };
 
-  inherit passthruVendoredSbom;
-
-}
+  buildBom = self.callPackage ./nix/build-bom.nix {
+    inherit (self.__internal) buildtimeDependencies runtimeDependencies;
+  };
+})
